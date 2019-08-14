f8c0e9f80cea75c8895023b0b78e6d31459e0fb3
Ilias <fatkullin_ilia@mail.ru>
2019-08-15 00:04:05 +0300

    Build setup with webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
+};
